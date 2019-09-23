ade03cb13116ee01ce44653ab372c495440599f7
Dune Zerna <duzer17@student.sdu.dk>
2019-09-23 15:08:36 +0200

    Layout & Element change

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -1,37 +1,52 @@
- $(function () {
-     var documentEl = $(document),
-         fadeElem = $('.fade-scroll');
+$(document).ready(function(){
+   
+    var introStringArray = ["P","R","O","C","T","O","R","S","O","L","U","T","I","O","N","S","?"];
+    
+    introStringArray.forEach(function(item) {
+       $(".intro_text").append("<li>" + item + "</li>")
+    });
+    
+});
 
-     documentEl.on('scroll', function () {
-         var currScrollPos = documentEl.scrollTop();
+// Fade when scrolling function
 
-         fadeElem.each(function () {
-             var $this = $(this),
-                 elemOffsetTop = $this.offset().top;
-             if (currScrollPos > elemOffsetTop) {
-                 $this.css('opacity', 1 - (currScrollPos - elemOffsetTop) / 300);
-                 $this.css('zIndex', '60');
-             } else if (currScrollPos < elemOffsetTop){
-                 $this.css('opacity', 1 + (currScrollPos - elemOffsetTop) / 900);
-                 $this.css('zIndex', '1');
-             }
+$(function () {
+    var documentEl = $(document),
+        fadeElem = $('.fade-scroll');
+
+    documentEl.on('scroll', function () {
+        var currScrollPos = documentEl.scrollTop();
+
+        fadeElem.each(function () {
+            var $this = $(this),
+                elemOffsetTop = $this.offset().top;
+            if (currScrollPos > elemOffsetTop) {
+                $this.css('opacity', 1 - (currScrollPos - elemOffsetTop) / 400);
+                $this.css('zIndex', '60');
+                setTimeout(function () {
+                    $('.fly-in-text').removeClass('hidden');
+                }, 500);
+            }
 
 
-         });
-     });
- });
+        });
+    });
+});
 
- $(function () {
-     var documentEl2 = $(document),
-         fadeElem2 = $('.fade-scroll2');
+// Fade when scrolling function variation
 
-     documentEl2.on('scroll', function () {
-         var currScrollPos2 = documentEl2.scrollTop();
+$(function () {
+    var documentEl2 = $(document),
+        fadeElem2 = $('.fade-scroll2');
 
-         fadeElem2.each(function () {
-             var $this = $(this),
-                 elemOffsetTop2 = $this.offset().top;
-             if (currScrollPos2 > elemOffsetTop2) $this.css('opacity', 1 - (currScrollPos2 - elemOffsetTop2) / 30)
-         });
-     });
- });
+    documentEl2.on('scroll', function () {
+        var currScrollPos2 = documentEl2.scrollTop();
+
+        fadeElem2.each(function () {
+            var $this = $(this),
+                elemOffsetTop2 = $this.offset().top;
+            if (currScrollPos2 > elemOffsetTop2) $this.css('opacity', 1 - (currScrollPos2 - elemOffsetTop2) / 30)
+        });
+    });
+});
+
